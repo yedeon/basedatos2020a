514b3862d632e1475053d8515e5d436992afecde
yedeon <59893384+yedeon@users.noreply.github.com>
2020-01-14 16:04:08 -0600

    Add files via upload

---
+++ db_config.php
@@ -0,0 +1,6 @@
+<?php
+	define('DB_USER', "id681993_yedeon12"); // Usuario
+	define('DB_PASSWORD', "semenor1"); // Password
+	define('DB_DATABASE', "id681993_yedeon12"); // Nombre de la base de datos
+	define('DB_SERVER', "localhost"); // db server
+	?>
