a8a5d2238fb680ccb88d8f82a8a01b2d3b444c07
yedeon <59893384+yedeon@users.noreply.github.com>
2020-01-14 22:34:34 -0600

    Add files via upload

---
+++ Producto_INSERTAR_POST.php
@@ -0,0 +1,44 @@
+<?php
+	require 'SQLGlobal.php';
+
+	if($_SERVER['REQUEST_METHOD']=='POST'){
+		try{
+			$datos = json_decode(file_get_contents("php://input"),true);
+
+            $id = $datos["id"]; // obtener parametros POST
+            $descripcion = $datos["descripcion"];
+            $precio = $datos["precio"];
+            $categoria = $datos["categoria"];
+			//$respuesta = SQLGlobal::query("QUERY");//sin filtro ("No incluir filtros ni '?'")
+			$respuesta = SQLGlobal::cudFiltro(
+				"insert into bd2 values(?,?,?,?)",
+				array($id,$descripcion,$precio,$categoria)
+			);//con filtro ("El tamaño del array debe ser igual a la cantidad de los '?'")
+            if($respuesta>0){
+                echo json_encode(array(
+                    'respuesta'=>'200',
+                    'estado' => 'Se inserto ok',
+                    'data'=>'Nro registros afectados son: '.$respuesta,
+                    'error'=>''
+                ));
+            }else{
+                echo json_encode(array(
+                    'respuesta'=>'100',
+                    'estado' => 'No se registro',
+                    'data'=>'Nro registros afectados son: '.$respuesta,
+                    'error'=>''
+                ));
+            }
+            
+		}catch(PDOException $e){
+			echo json_encode(
+				array(
+					'respuesta'=>'-1',
+					'estado' => 'Ocurrio un error, intentelo mas tarde',
+					'data'=>'',
+					'error'=>$e->getMessage())
+			);
+		}
+	}
+
+?>
