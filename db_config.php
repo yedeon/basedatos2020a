3f976aaee5bbf37b3c61238483bf8b9ebfb41b54
yedeon <59893384+yedeon@users.noreply.github.com>
2020-01-14 18:03:49 -0600

    Add files via upload

--- db_config.php
+++ db_config.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_USER', "nymfruekertrzs"); // Usuario
-define('DB_PASSWORD', "bf39f16d1394414ceb824a3e23fdd6321aca423409b49269bed74e7bdd333b86"); // Password
-define('DB_DATABASE', "d2fg8il1hct838"); // Nombre de la base de datos
-define('DB_SERVER', "ec2-54-225-121-235.compute-1.amazonaws.com"); //host server
+define('DB_USER', "kvroxolmlcqfgb"); // Usuario
+define('DB_PASSWORD', "47bd6afda672b672b41887acbee02ae16bad469fca70c7581d723875974a83c3"); // Password
+define('DB_DATABASE', "d614viiu0ih5q1"); // Nombre de la base de datos
+define('DB_SERVER', "ec2-3-224-165-85.compute-1.amazonaws.com"); //host or server
 ?>
