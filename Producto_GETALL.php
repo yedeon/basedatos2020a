60e9b93d9a6c1ae423dc2ffd1961e50b5dac5368
yedeon <59893384+yedeon@users.noreply.github.com>
2020-01-16 22:46:01 -0600

    Add files via upload

--- Producto_GETALL.php
+++ Producto_GETALL.php
@@ -4,13 +4,13 @@
 	if($_SERVER['REQUEST_METHOD']=='GET'){
 		try{
 			$respuesta = SQLGlobal::selectArray("select * from bd2"); // semodifica esta linea decodigo
-			// echo json_encode(array(
-			// 	'respuesta'=>'200',
-			// 	'estado' => 'Se obtuvieron los datos correctamente',
-			// 	'data'=>$respuesta,
-			// 	'error'=>''
-			// )
-			echo "logeado okss";
+			echo json_encode(array(
+				'respuesta'=>'200',
+				'estado' => 'Se obtuvieron los datos correctamente',
+				'data'=>$respuesta,
+				'error'=>''
+			)
+			//echo "logeado okss";
 			);
 		}catch(PDOException $e){
 			echo json_encode(
