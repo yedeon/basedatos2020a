40483520e50329283e9db43413b9122a4132f4d6
yedeon <59893384+yedeon@users.noreply.github.com>
2020-01-20 17:40:57 -0600

    Add files via upload

--- HGetUsersGetF.php
+++ HGetUsersGetF.php
@@ -3,38 +3,26 @@
 
 	if($_SERVER['REQUEST_METHOD']=='GET'){
 		try{
-			$datos = json_decode(file_get_contents("php://input"),true);
+			//$datos = json_decode(file_get_contents("php://input"),true);
 
             $id = $datos["id"]; // obtener parametros POST
 			$respuesta = SQLGlobal::selectArrayFiltro("SELECT * FROM bd2  WHERE id=?",
 				array($id) 
 			);//con filtro ("El tamaño del array debe ser igual a la cantidad de los '?'")
-            if($respuesta>0){
-                echo json_encode(array(
+            echo json_encode(array(
                     'respuesta'=>'200',
                     'estado' => 'Se Filtro ok',
                     'data'=>'Nro registros afectados son: '.$respuesta, //. concatea en php // en +
                     'error'=>''
                 ));
-            }else{
+            }catch(PDOException $e){
                 echo json_encode(array(
-                    'respuesta'=>'100',
+                    'respuesta'=>'-1',
                     'estado' => 'No EXISTE',
-                    'data'=>'Nro registros afectados son: '.$respuesta,
-                    'error'=>''
-                ));
-            }
-            
-		}catch(PDOException $e){
-			echo json_encode(
-				array(
-					'respuesta'=>'-1',
-					'estado' => 'Ocurrio un error, intentelo mas tarde',
-					'data'=>'',
-					'error'=>$e->getMessage())
+                    'data'=>'',
+                    'error'=>$e->getMessage())
+		
 			);
 		}
 	}
 ?>
-
-<?php
