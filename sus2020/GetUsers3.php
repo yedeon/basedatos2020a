ac1ba52d3d0ec6d14694edbac53f27a3801dc5aa
yedeon <59893384+yedeon@users.noreply.github.com>
2020-01-20 13:29:38 -0600

    Add files via upload

--- sus2020/GetUsers3.php
+++ sus2020/GetUsers3.php
@@ -1,16 +1,18 @@
 <?php
             //$servername = "WSTIENDA";
-            $servername ="ec2-3-224-165-85.compute-1.amazonaws.com";
-            $username = "kvroxolmlcqfgb";
-            $password = "2015aaa";
-            $dbname = "d614viiu0ih5q1";
+            $servername ="ec2-3-224-165-85.compute-1.amazonaws.com"; //host name adress
+            $username = "kvroxolmlcqfgb"; //user
+            $password = "47bd6afda672b672b41887acbee02ae16bad469fca70c7581d723875974a83c3"; //password
+            $dbname = "d614viiu0ih5q1"; //database
+            //"apikey:fa666d0c-fcda-48b1-aac4-16bdfd437280"
+            //"token autorizacion:"Heroku CLI"  "fa666d0c-fcda-48b1-aac4-16bdfd437280"
 
             $id=$_POST["id"];
             $descripcion=$_POST["descripcion"];
 
             $conn = new mysqli($servername, $username, $password, $dbname);
             if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
+                die("Connection failed1: " . $conn->connect_error);
                 echo "conectado Error";
             }
                 echo "conectado con exito.<br></br>";
