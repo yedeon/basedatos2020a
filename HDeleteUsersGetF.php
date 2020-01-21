26daabb60898e697f775bd2b0735293f53021577
yedeon <59893384+yedeon@users.noreply.github.com>
2020-01-20 22:12:40 -0600

    Add files via upload

--- HDeleteUsersGetF.php
+++ HDeleteUsersGetF.php
@@ -5,26 +5,30 @@
 		try{
 			$datos = json_decode(file_get_contents("php://input"),true);
 
-            //(int)$id = $datos["id"]; // obtener parametros POST
-            $ids =11;
+            (int)$id = $datos["id"]; // obtener parametros POST
+            //$ids =11;
 			$respuesta = SQLGlobal::cudFiltro(
                 "DELETE FROM bd2  WHERE id =?",
 				array($ids) 
-			);//con filtro ("El tamaño del array debe ser igual a la cantidad de los '?'")
-            echo json_encode(array(
-                    'respuesta'=>'200',
-                    'estado' => 'Se Borro ok',
-                    'data'=>$respuesta, //. concatea en php // en +
-                    'error'=>''
-                ));
+            );//con filtro ("El tamaño del array debe ser igual a la cantidad de los '?'")
+            echo($id)
+            // echo json_encode(array(
+            //         'respuesta'=>'200',
+            //         'estado' => 'Se Borro ok',
+            //         'data'=>$respuesta, //. concatea en php // en +
+            //         'error'=>''
+            //     ))
+            ;
             }catch(PDOException $e){
-                echo json_encode(array(
-                    'respuesta'=>'-1',
-                    'estado' => 'No EXISTE',
-                    'data'=>'',
-                    'error'=>$e->getMessage())
+                echo($id)
+                // echo json_encode(array(
+                //     'respuesta'=>'-1',
+                //     'estado' => 'No EXISTE',
+                //     'data'=>'',
+                //     'error'=>$e->getMessage())
 		
-			);
+                // )
+                ;
 		    }
 	    }
 ?>
