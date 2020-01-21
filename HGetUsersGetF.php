b3e12b18e0a37184ea52a8a84c9f4e04bde5984c
yedeon <59893384+yedeon@users.noreply.github.com>
2020-01-20 18:23:26 -0600

    Add files via upload

--- HGetUsersGetF.php
+++ HGetUsersGetF.php
@@ -5,9 +5,9 @@
 		try{
 			//$datos = json_decode(file_get_contents("php://input"),true);
 
-            $id = $$_GET["id"]; // obtener parametros POST
+           // $id = $$_GET["id"]; // obtener parametros POST
 			$respuesta = SQLGlobal::selectArrayFiltro(
-                "SELECT * FROM bd2  WHERE id>2",
+                "SELECT * FROM bd2  WHERE id > 2",
 				array($id) 
 			);//con filtro ("El tamaño del array debe ser igual a la cantidad de los '?'")
             echo json_encode(array(
