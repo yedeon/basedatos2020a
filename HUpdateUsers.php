aec05dfa9afa3a656fda27df1d3c2c5971ab7ecb
yedeon <59893384+yedeon@users.noreply.github.com>
2020-01-21 02:44:11 -0600

    Add files via upload

--- HUpdateUsers.php
+++ HUpdateUsers.php
@@ -1,40 +1,42 @@
 <?php
-require 'SQLGlobal.php';
+	require 'SQLGlobal.php';
 
-if($_SERVER['REQUEST_METHOD']=='POST'){
-    try{
-        $datos = json_decode(file_get_contents("php://input"),true);
+	if($_SERVER['REQUEST_METHOD']=='POST'){
+		try{
+			$datos = json_decode(file_get_contents("php://input"),true);
 
-        (int)$id = $datos["id"]; // obtener parametros POST
-        $descripcion = $datos["descripcion"]; 
-        (int)$precio = $datos["precio"]; 
-        $categoria = $datos["categoria"]; 
-        $respuesta = SQLGlobal::cudFiltro(
-            "UPDATE bd2 SET descripcion =?,precio=?,categoria=? WHERE id=?",
-            array($descripcion,$precio,$categoria,$id)
-        );//con filtro ("El tamaño del array debe ser igual a la cantidad de los '?'")
-        if($respuesta>0){
-            echo json_encode(array(
-                'respuesta'=>'200',
-                'estado' => 'Se Update ok',
-                'data'=>'Nro Filas afectadas; '.$respuesta,
-                'error'=>''
-            ));
-        }else{
-            echo json_encode(array(
-                'respuesta'=>'100',
-                'estado' => 'No Update ok',
-                'data'=>'Nro Filas afectadas; '.$respuesta,
-                'error'=>''
-            ));
-        }
-        }catch(PDOException $e){
-        echo json_encode(array(
-                'respuesta'=>'-1',
-                'estado' => 'Ocurrio un error, intentelo mas tarde',
-                'data'=>'',
-                'error'=>$e->getMessage())
-            );
-        }
-    }
+            $id = $datos["id"]; // obtener parametros GET
+            $descripcion = $datos["descripcion"];
+            $precio = $datos["precio"];
+            $categoria = $datos["categoria"];
+			$respuesta = SQLGlobal::cudFiltro(
+				"UPDATE Producto SET descripcion = ?, precio = ?, categoria = ? WHERE id = ?",
+				array($descripcion,$precio,$categoria,$id)
+            );//con filtro ("El tamaño del array debe ser igual a la cantidad de los '?'")
+            if($respuesta>0){
+                echo json_encode(array(
+                    'respuesta'=>'200',
+                    'estado' => 'Se actualizo correctamente el producto',
+                    'data'=>'Numero de filas afectadas: '.$respuesta,
+                    'error'=>''
+                ));
+            }else{
+                echo json_encode(array(
+                    'respuesta'=>'100',
+                    'estado' => 'No se actualizo correctamente.',
+                    'data'=>'Numero de filas afectadas: '.$respuesta,
+                    'error'=>''
+                ));
+            }
+		}catch(PDOException $e){
+			echo json_encode(
+				array(
+					'respuesta'=>'-1',
+					'estado' => 'Ocurrio un error, intentelo mas tarde',
+					'data'=>'',
+					'error'=>$e->getMessage())
+			);
+		}
+	}
+
 ?>
