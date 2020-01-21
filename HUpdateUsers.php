971d6113a0342fa4c4d030a7e9a6d20fb8c293a0
yedeon <59893384+yedeon@users.noreply.github.com>
2020-01-21 01:58:11 -0600

    Add files via upload

---
+++ HUpdateUsers.php
@@ -0,0 +1,50 @@
+<?php
+require 'SQLGlobal.php';
+
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    try{
+        $datos = json_decode(file_get_contents("php://input"),true);
+
+        (int)$id = $_POST["id"]; // obtener parametros POST
+        $descripcion = $_POST["descripcion"]; 
+        (int)$precio = $_POST["precio"]; 
+        $categoria = $_POST["categoria"]; 
+        //$ids =11;
+        $respuesta = SQLGlobal::cudFiltro("UPDATE bd2 SET id=8,descripcion =1,precio=1,categoria=1 WHERE id=1");
+           // array((int)$id) 
+        //con filtro ("El tamaño del array debe ser igual a la cantidad de los '?'")
+        echo json_encode(array(
+                'respuesta'=>'200',
+                'estado' => 'Se Borro ok',
+                'data'=>$respuesta, //. concatea en php // en +
+                'error'=>''
+            ));
+        }catch(PDOException $e){
+            echo json_encode(array(
+                'respuesta'=>'-1',
+                'estado' => 'No EXISTE',
+                'data'=>'',
+                'error'=>$e->getMessage()
+            ));
+        }
+    }
+?>
+
+try{
+                    $datos = json_decode(file_get_contents("php://input"),true);
+
+                    (int)$id = $_POST["id"]; // obtener parametros POST
+                    $descripcion = $_POST["descripcion"]; 
+                    (int)$precio = $_POST["precio"]; 
+                    $categoria = $_POST["categoria"]; 
+                    //$ids =11;
+                    $respuesta = SQLGlobal::cudFiltro(
+                        "INSERT INTO bd2  values (?,?,?,?)",
+                        array($id,$descripcion,$precio,$categoria));//con filtro ("El tamaño del array debe ser igual a la cantidad de los '?'")
+                            if($respuesta>0){
+                                echo json_encode(array(
+                                    'respuesta'=>'200',
+                                    'estado' => 'Se inserto ok',
+                                    'data'=>'Nro registros afectados son: '.$respuesta,
+                                    'error'=>''
+                                ));
