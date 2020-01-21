a1432ff7eedd74de19d767c137203c1de9174a70
yedeon <59893384+yedeon@users.noreply.github.com>
2020-01-21 02:19:29 -0600

    Add files via upload

--- HUpdateUsers.php
+++ HUpdateUsers.php
@@ -5,20 +5,29 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
     try{
         $datos = json_decode(file_get_contents("php://input"),true);
 
-        (int)$id = $_POST["id"]; // obtener parametros POST
-        $descripcion = $_POST["descripcion"]; 
-        (int)$precio = $_POST["precio"]; 
-        $categoria = $_POST["categoria"]; 
+        (int)$id = $datos["id"]; // obtener parametros POST
+        $descripcion = $datos["descripcion"]; 
+        (int)$precio = $datos["precio"]; 
+        $categoria = $datos["categoria"]; 
         //$ids =11;
-        $respuesta = SQLGlobal::cudFiltro("UPDATE bd2 SET id=? ,descripcion =?,precio=?,categoria=? WHERE id=?");
-            array((int)$id,$descripcion,$precio,$categoria) ;
-        //con filtro ("El tamaño del array debe ser igual a la cantidad de los '?'")
-        echo json_encode(array(
+        $respuesta = SQLGlobal::cudFiltro("UPDATE bd2 SET id=? ,descripcion =?,precio=?,categoria=? WHERE id=?",
+            array((int)$id,$descripcion,$precio,$categoria)
+        );//con filtro ("El tamaño del array debe ser igual a la cantidad de los '?'")
+        if($respuesta>0){
+            echo json_encode(array(
                 'respuesta'=>'200',
-                'estado' => 'Se Borro ok',
-                'data'=>$respuesta, //. concatea en php // en +
+                'estado' => 'Se Update ok',
+                'data'=>'Nro Filas afectadas; '.$respuesta,
                 'error'=>''
             ));
+        }else{
+            echo json_encode(array(
+                'respuesta'=>'100',
+                'estado' => 'No Update ok',
+                'data'=>'Nro Filas afectadas; '.$respuesta,
+                'error'=>''
+            ));
+        }
         }catch(PDOException $e){
             echo json_encode(array(
                 'respuesta'=>'-1',
@@ -29,22 +38,3 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
         }
     }
 ?>
-
-try{
-                    $datos = json_decode(file_get_contents("php://input"),true);
-
-                    (int)$id = $_POST["id"]; // obtener parametros POST
-                    $descripcion = $_POST["descripcion"]; 
-                    (int)$precio = $_POST["precio"]; 
-                    $categoria = $_POST["categoria"]; 
-                    //$ids =11;
-                    $respuesta = SQLGlobal::cudFiltro(
-                        "INSERT INTO bd2  values (?,?,?,?)",
-                        array($id,$descripcion,$precio,$categoria));//con filtro ("El tamaño del array debe ser igual a la cantidad de los '?'")
-                            if($respuesta>0){
-                                echo json_encode(array(
-                                    'respuesta'=>'200',
-                                    'estado' => 'Se inserto ok',
-                                    'data'=>'Nro registros afectados son: '.$respuesta,
-                                    'error'=>''
-                                ));
