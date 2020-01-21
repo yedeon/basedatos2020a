46a65ed052f79015c87120ca87e4b86476eceb12
yedeon <59893384+yedeon@users.noreply.github.com>
2020-01-21 02:39:28 -0600

    Add files via upload

--- HUpdateUsers.php
+++ HUpdateUsers.php
@@ -9,9 +9,9 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
         $descripcion = $datos["descripcion"]; 
         (int)$precio = $datos["precio"]; 
         $categoria = $datos["categoria"]; 
-        //$ids =11;
-        $respuesta = SQLGlobal::cudFiltro("UPDATE bd2 SET descripcion =?,precio=?,categoria=? WHERE id=?",
-            array($descripcion,(int)$precio,$categoria,(int)$id)
+        $respuesta = SQLGlobal::cudFiltro(
+            "UPDATE bd2 SET descripcion =?,precio=?,categoria=? WHERE id=?",
+            array($descripcion,$precio,$categoria,$id)
         );//con filtro ("El tamaño del array debe ser igual a la cantidad de los '?'")
         if($respuesta>0){
             echo json_encode(array(
@@ -29,12 +29,12 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
             ));
         }
         }catch(PDOException $e){
-            echo json_encode(array(
+        echo json_encode(array(
                 'respuesta'=>'-1',
-                'estado' => 'No EXISTE',
+                'estado' => 'Ocurrio un error, intentelo mas tarde',
                 'data'=>'',
-                'error'=>$e->getMessage()
-            ));
+                'error'=>$e->getMessage())
+            );
         }
     }
 ?>
