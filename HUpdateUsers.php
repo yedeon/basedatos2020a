8911b22fa84ab754044f4fdbba370ad607229525
yedeon <59893384+yedeon@users.noreply.github.com>
2020-01-21 13:37:21 -0600

    Add files via upload

--- HUpdateUsers.php
+++ HUpdateUsers.php
@@ -14,28 +14,31 @@
 				array($id,$descripcion,$precio,$categoria)
             );//con filtro ("El tamaño del array debe ser igual a la cantidad de los '?'")
             if($respuesta>0){
-                echo json_encode(array(
-                    'respuesta'=>'200',
-                    'estado' => 'Se actualizo correctamente el producto',
-                    'data'=>'Numero de filas afectadas: '.$respuesta,
-                    'error'=>''
-                ));
+                // echo json_encode(array(
+                //     'respuesta'=>'200',
+                //     'estado' => 'Se actualizo correctamente el producto',
+                //     'data'=>'Numero de filas afectadas: '.$respuesta,
+                //     'error'=>''
+                // ));
+                echo("1 ".$id." ".$descripcion." ".$precio." ".$categoria);
             }else{
-                echo json_encode(array(
-                    'respuesta'=>'100',
-                    'estado' => 'No se actualizo correctamente.',
-                    'data'=>'Numero de filas afectadas: '.$respuesta,
-                    'error'=>''
-                ));
+                // echo json_encode(array(
+                //     'respuesta'=>'100',
+                //     'estado' => 'No se actualizo correctamente.',
+                //     'data'=>'Numero de filas afectadas: '.$respuesta,
+                //     'error'=>''
+                // ));
+                echo("2 ".$id." ".$descripcion." ".$precio." ".$categoria);
             }
 		}catch(PDOException $e){
-			echo json_encode(
-				array(
-					'respuesta'=>'-1',
-					'estado' => 'Ocurrio un error, intentelo mas tarde',
-					'data'=>'',
-					'error'=>$e->getMessage())
-			);
+			// echo json_encode(
+			// 	array(
+			// 		'respuesta'=>'-1',
+			// 		'estado' => 'Ocurrio un error, intentelo mas tarde',
+			// 		'data'=>'',
+			// 		'error'=>$e->getMessage())
+            // );
+            echo("3 ".$id." ".$descripcion." ".$precio." ".$categoria);
 		}
 	}
 
