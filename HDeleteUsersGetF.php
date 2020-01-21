c0ff096f0407af980fd66158492e1fec297c93a5
yedeon <59893384+yedeon@users.noreply.github.com>
2020-01-20 20:26:24 -0600

    Add files via upload

--- HDeleteUsersGetF.php
+++ HDeleteUsersGetF.php
@@ -6,7 +6,7 @@
 			$datos = json_decode(file_get_contents("php://input"),true);
 
             $id = $datos["id"]; // obtener parametros POST
-			$respuesta = SQLGlobal::cudFiltro(
+			$respuesta = SQLGlobal::cud(
                 "DELETE FROM bd2  WHERE id =?",
 				array($id) 
 			);//con filtro ("El tamaño del array debe ser igual a la cantidad de los '?'")
