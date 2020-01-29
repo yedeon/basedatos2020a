8b71c4a32393a759bc88baf60e844050399e7ce8
yedeon <59893384+yedeon@users.noreply.github.com>
2020-01-28 22:41:31 -0600

    Add files via upload

---
+++ bd3/HUpdateUsersP.php
@@ -0,0 +1,43 @@
+<?php
+	require 'SQLGlobal.php';
+
+	if($_SERVER['REQUEST_METHOD']=='POST'){
+		try{
+			$datos= json_decode(file_get_contents("php://input"),true);
+
+            
+            $descripcion=$_POST["descripcion"];
+            $precio=$_POST["precio"];
+            $categoria=$_POST["categoria"]; 
+            (int)$id=$_POST["id"]; // obtener parametros GET
+			$respuesta = SQLGlobal::cudFiltro(
+				"UPDATE bd3 SET descripcion=?,precio=?,categoria=? WHERE id=?",
+				array($descripcion,$precio,$categoria,$id)//renglones descen y dercha mismo orden de $xx
+            );//con filtro ("El tamaño del array debe ser igual a la cantidad de los '?'")
+            if($respuesta>0){
+                echo json_encode(array(
+                    'respuesta'=>'200',
+                    'estado' => 'Se actualizo correctamente el producto',
+                    'data'=>'Numero de filas afectadas: '.$respuesta,
+                    'error'=>''
+                ));
+            }else{
+                echo json_encode(array(
+                    'respuesta'=>'100',
+                    'estado' => 'No se actualizo correctamente.',
+                    'data'=>'Numero de filas afectadas: '.$respuesta,
+                    'error'=>''
+                ));
+            }
+		}catch(PDOException $e){
+			echo json_encode(
+				array(
+					'respuesta'=>'-1',
+					'estado' => 'Ocurrio un error, intentelo mas tarde',
+					'data'=>'',
+					'error'=>$e->getMessage())
+            );
+		}
+	}
+
+?>
