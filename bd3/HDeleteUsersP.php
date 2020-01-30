13c19ebf9a9662a46fe92d420f7a255e82fde82e
yedeon <59893384+yedeon@users.noreply.github.com>
2020-01-29 22:55:15 -0600

    Add files via upload

--- bd3/HDeleteUsersP.php
+++ bd3/HDeleteUsersP.php
@@ -5,9 +5,9 @@
 		try{
 			$datos = json_decode(file_get_contents("php://input"),true);
 
-            $id = $datos["id"]; // obtener parametros POST
-			$respuesta = SQLGlobal::cudFiltro("DELETE FROM bd3  WHERE id=?",
-				array($id) 
+            $descripcion = $datos["descripcion"]; // obtener parametros POST
+			$respuesta = SQLGlobal::cudFiltro("DELETE FROM bd3  WHERE descripcion=?",
+				array($descripcion) 
 			);//con filtro ("El tamaño del array debe ser igual a la cantidad de los '?'")
             if($respuesta>0){
                 echo json_encode(array(
