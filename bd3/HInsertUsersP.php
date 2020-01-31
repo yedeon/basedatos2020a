c10bf18bd159c5d29f213dcc734f6498df6d316d
yedeon <59893384+yedeon@users.noreply.github.com>
2020-01-31 00:55:30 -0600

    Add files via upload

--- bd3/HInsertUsersP.php
+++ bd3/HInsertUsersP.php
@@ -10,8 +10,8 @@
             $precio = $_POST["precio"]; 
             $categoria = $_POST["categoria"]; 
             if( is_null($id)){
-                $respuesta = SQLGlobal::cudFiltro("INSERT INTO bd3 values (?,?,?)",
-                array($descripcion,$precio,$categoria));
+                $respuesta = SQLGlobal::cudFiltro("INSERT INTO bd3 values (?,?,?,?)",
+                array('',$descripcion,$precio,$categoria));
             }else{
                 $respuesta = SQLGlobal::cudFiltro("INSERT INTO bd3 values (?,?,?,?)",
                 array($id,$descripcion,$precio,$categoria));
