76711d90a1025edfc5a749abdfdbf2c08e0e18a2
yedeon <59893384+yedeon@users.noreply.github.com>
2020-03-28 23:45:04 -0600

    Add files via upload

--- 230320_19/bd4/HGetUsersAllCountP.php
+++ 230320_19/bd4/HGetUsersAllCountP.php
@@ -1,34 +1,3 @@
-<!-- <?php
-	require 'SQLGlobal.php';
-
-	if($_SERVER['REQUEST_METHOD']=='POST'){
-		try{
-			$datos = json_decode(file_get_contents("php://input"),true);
-
-			$nroemp = $datos[4]; // obtener parametros GET
-			//$respuesta = SQLGlobal::query("QUERY");//sin filtro ("No incluir filtros ni '?'")
-			$respuesta = SQLGlobal::selectObject(
-				"QUERY WHERE nroemp="&4&'"',
-				array("nroemp")
-			);//con filtro ("El tamaño del array debe ser igual a la cantidad de los '?'")
-			echo json_encode(array(
-				'respuesta'=>'200',
-				'estado' => 'Se obtuvieron los datos correctamente',
-				'data'=>$respuesta,
-				'error'=>''
-			));
-		}catch(PDOException $e){
-			echo json_encode(
-				array(
-					'respuesta'=>'-1',
-					'estado' => 'Ocurrio un error, intentelo mas tarde',
-					'data'=>'',
-					'error'=>$e->getMessage())
-			);
-		}
-	}
-
-?> -->
 
 <?php
             require 'SQLGlobal.php';
